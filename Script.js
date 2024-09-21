c7cc06fe583a34065a5d9037118d6dad35822c95
diegodealer <diego.rf726@gmail.com>
2024-09-21 17:10:01 -0600

    checkbox oculta+colores dependiendo si son pares o nones

--- Script.js
+++ Script.js
@@ -6,9 +6,42 @@ function addTask() {
         let li = document.createElement("li");
         let checkbox  = document.createElement("Input");
         checkbox.type =  "checkbox";
-        checkbox.className = "taskCheckbox";
+        checkbox.style.display = "none";
+        checkbox.addEventListener('change', function() {
+            if (checkbox.checked) {
+                taskText.style.textDecoration = 'underline'; 
+            } else {
+                taskText.style.textDecoration = 'none'; 
+            }
+        });
+
+        let taskText = document.createElement("Span");
+        taskText.textContent = taskInput.value;
+        taskText.addEventListener('click', function() {
+            checkbox.checked = !checkbox.checked; 
+            if (checkbox.checked) {
+                taskText.style.textDecoration = 'underline'; 
+            } else {
+                taskText.style.textDecoration = 'none'; 
+            }
+        });
+
+        checkbox.addEventListener('change', function() {
+            if (checkbox.checked) {
+                taskText.style.textDecoration = 'underline'; 
+            } else {
+                taskText.style.textDecoration = 'none'; 
+            }
+        });
+
         li.appendChild(checkbox);
-        li.appendChild(document.createTextNode(taskInput.value));
+        li.appendChild(taskText);
+        let index = tasklist.children.length;
+        if((index + 1)% 2 === 0){
+            li.style.color = "blue";
+        } else {
+            li.style.color = "black";
+        }
         tasklist.appendChild(li);
         taskInput.value = "";
     }
@@ -18,11 +51,10 @@ function clearSelectedTasks() {
     let taskList = document.getElementById("taskList");
     let tasks = taskList.getElementsByTagName("li");
 
-    // Recorrer las tareas en reversa para evitar problemas al eliminar elementos
     for (let i = tasks.length - 1; i >= 0; i--) {
-        let checkbox = tasks[i].getElementsByTagName("input")[0]; // obtener el checkbox dentro de cada 'li'
+        let checkbox = tasks[i].getElementsByTagName("input")[0]; 
         if (checkbox.checked) {
-            taskList.removeChild(tasks[i]); // eliminar el 'li' si el checkbox está seleccionado
+            taskList.removeChild(tasks[i]); 
         }
     }
 }
